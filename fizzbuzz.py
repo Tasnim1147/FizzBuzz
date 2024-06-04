0f186605b47856f379d07f03562ffb78247706c9
Tasnim1147 <tsayem1147@gmail.com>
2024-06-04 12:20:53 +0300

    Implemented and Refactor test fizz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,10 @@
 
 
-
-def fizzbuzz(n: int) :
+def process(n: int) -> str:
+    if (n % 3 == 0): return "Fizz"
     return n
+
+
+def fizzbuzz() :
+    
+    for num in range(1, 101): print(process(num))
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,5 +1,19 @@
-from fizzbuzz import fizzbuzz
+from fizzbuzz import process
 
 
 def test_one():
-    assert(fizzbuzz(1) == 1)
+    assert(process(1) == 1)
+    
+    
+def test_simple_numbers():
+    
+    assert(process(2) == 2)
+    assert(process(5) == 5)
+    assert(process(7) == 7)
+    
+    
+def test_fizz_exclusive():
+    
+    assert(process(3) == "Fizz")
+    assert(process(6) == "Fizz")
+    assert(process(9) == "Fizz")
